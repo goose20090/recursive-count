edab484cb76a51c7c7559e0c657673653d27cf3e
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-01-18 11:12:47 +0000

    commit tree 7f24192f4f30530c7b15a7f9e96446ffa3c3fb85

--- ruby/recursive_count.rb
+++ ruby/recursive_count.rb
@@ -1,5 +1,9 @@
 def recursive_count(num = 0)
-  # type your code in here
+  # base case the last recursive call will return itself first
+  return if num == 10
+  puts num
+  num += 1
+  recursive_count(num)
 end
 
 if __FILE__ == $PROGRAM_NAME
